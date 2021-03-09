ffcf426e0f2d492b97bf6d024ada4feb06fe3fc4
vivien <826197021@qq.com>
2021-03-09 12:32:21 +0800

    更新联动限定

--- modules/gameData.py
+++ modules/gameData.py
@@ -13,14 +13,15 @@ with open('resource/config/update_config.json', encoding='utf-8', mode='r')as fi
 high_star = update_config['high_star']
 classes = update_config['classes']
 types = update_config['types']
-limit = update_config['limit']
+limit = update_config['limit'] + update_config['linkage']
 unavailable = [item for item in
                update_config['unavailable']['not_normally'] +
                update_config['unavailable']['sale_only'] +
                update_config['unavailable']['recruit_only'] +
                update_config['unavailable']['activity_only'] +
                update_config['unavailable']['contract_only'] +
-               update_config['unavailable']['roguelike_only']]
+               update_config['unavailable']['roguelike_only'] +
+               update_config['unavailable']['linkage_only']]
 
 
 class Operator:
