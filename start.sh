985d9d1522ba4141647467c48bd47244f267a7f2
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-09-19 00:35:17 +0800

    测试时临时去除clear

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 stty -echo
 pip3 config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/slmple/
-clear
 pip3 install -r requirements.txt
-clear
 python3 amiya.py
-clear
 stty echo
