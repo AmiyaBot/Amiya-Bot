3636daa9207355442d5781c878b8f1087363d465
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-09-18 22:03:13 +0800

    启动前自动安装依赖，校时以确保日志时间准确无误

--- strat.cmd
+++ strat.cmd
@@ -1,3 +1,17 @@
 @echo off
+cd %~dp0
+net start w32time
+cls
+w32tm /config /manualpeerlist:"ntp.ntsc.ac.cn" /syncfromflags:manual /reliable:yes /update
+ping -n 2 localhost >nul
+cls
+w32tm /resync
+w32tm /resync
+w32tm /resync
+ping -n 2 localhost >nul
+cls
+pip install -r requirements.txt
+cls
 python amiya.py
+cls
 exit
