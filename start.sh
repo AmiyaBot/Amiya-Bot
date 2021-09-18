12f6f8cb0e5154669d010fef305548b4708e5a90
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-09-19 01:41:31 +0800

    重新加上clear

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 stty -echo
 pip3 config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/slmple/
+clear
 pip3 install -r requirements.txt
+clear
 python3 amiya.py
+clear
 stty echo
