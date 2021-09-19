0dc83e8889a4325e46b23e516ae9b3a54f428572
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-09-20 03:25:21 +0800

    添加校时（ntpdate，无系统检测）

--- start.sh
+++ start.sh
@@ -1,6 +1,22 @@
 stty -echo
 rm -rf start.cmd
 clear
+apt-get install ntpdate
+clear
+apt install ntpdate
+clear
+yum install ntp
+clear
+ntpdate ntp.ntsc.ac.cn
+clear
+ntpdate ntp.ntsc.ac.cn
+clear
+ntpdate ntp.ntsc.ac.cn
+clear
+ntpdate ntp.ntsc.ac.cn
+clear
+ntpdate ntp.ntsc.ac.cn
+clear
 pip3 config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/slmple/
 clear
 pip3 install -r requirements.txt
