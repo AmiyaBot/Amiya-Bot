99d30aaa7ea39eb31c871e80cc1b3b1faa6f7662
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-09-20 03:09:15 +0800

    自动删除start.sh
    不   忘   初   心

--- strat.cmd
+++ strat.cmd
@@ -1,6 +1,8 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
+del start.sh
+cls
 net start w32time
 cls
 w32tm /config /manualpeerlist:"ntp.ntsc.ac.cn" /syncfromflags:manual /reliable:yes /update
