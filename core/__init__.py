6f44050e34f65d29a56d61d9d4895085d4231577
vivien <826197021@qq.com>
2021-10-09 14:30:42 +0800

    解决数据库超时问题

--- core/__init__.py
+++ core/__init__.py
@@ -10,6 +10,7 @@ from core.network.httpRequests import MiraiHttp
 from core.resolver.message import Message
 from core.resolver.messageChain import Chain
 from core.database.manager import DataBase, Group, GroupSetting, Message as MessageBase
+from core.util.common import TimeRecorder
 from core.util.config import config
 from core.util import log
 
@@ -32,7 +33,9 @@ class AmiyaBot(WebSocket):
     def __save_message(self):
         while True:
             if self.message_stack:
+                t = TimeRecorder()
                 MessageBase.insert_many(self.message_stack).execute()
+                log.info(f'messages saved, total {len(self.message_stack)}, used {t.rec(millisecond=True)}ms')
                 self.message_stack = []
             time.sleep(30)
 
