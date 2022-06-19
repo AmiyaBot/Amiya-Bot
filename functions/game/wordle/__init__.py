454000937d562f700be194e2ab113023ef0e62c2
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2022-06-19 12:35:17 +0800

    新增举报
    你看我把不把你举办了就完事了

--- functions/game/wordle/__init__.py
+++ functions/game/wordle/__init__.py
@@ -1,8 +1,10 @@
 import asyncio
 import random
+import datetime
 
 from core import bot
-from core.database.user import UserInfo
+from core.config import config
+from core.database.messages import ReportRecord
 from functions.game.wordle.wordleBuilder import *
 
 
@@ -39,27 +41,36 @@ async def _(data: Message):
     await data.send(Chain(choice).text(f'{choice.text}难度，奖励倍数 {level_rate}'))
 
     while True:
+
         if not operators:
             operators = copy.deepcopy(ArknightsGameData().operators)
+
         operator = operators.pop(random.choice(list(operators.keys())))
         race = ''
+
         for story in operator.stories():
+
             if story['story_title'] == '基础档案':
                 r = re.search(r'\n【种族】.*?(\S+).*?\n', story['story_text'])
+
                 if r:
                     race = str(r.group(1))
+
         while not race or not operator.nation or '未知' in race or not operator.drawer_name:
             operator = operators.pop(random.choice(list(operators.keys())))
+
             for story in operator.stories():
+
                 if story['story_title'] == '基础档案':
                     r = re.search(r'\n【种族】.*?(\S+).*?\n', story['story_text'])
+
                     if r:
                         race = str(r.group(1))
 
         if curr != referee.count:
             curr = referee.count
-
             text = Chain(data, at=False).text(f'题目准备中...（{referee.count + 1}/{wordle_config.questions}）')
+
             if referee.user_ranking:
                 text.text('\n').text(calc_rank(referee)[0], auto_convert=False)
 
@@ -72,19 +83,23 @@ async def _(data: Message):
 
         if result.status in [WordleStatus.userClose, WordleStatus.systemClose]:
             end = True
+
         if result.status in [WordleStatus.userSkip, WordleStatus.systemSkip]:
             skip = True
+
         if result.status == WordleStatus.bingo:
             rewards = int(wordle_config.rewards.bingo * level_rate * (100 + result.total_point) / 100)
             UserInfo.add_jade_point(result.answer.user_id, rewards)
             set_rank(referee, result.answer, 1)
 
         if result.user_point:
+
             for user_id, point in result.user_point.items():
                 set_point(referee, user_id, point)
 
         if not skip:
             referee.count += 1
+
             if referee.count >= wordle_config.questions:
                 end = True
 
@@ -99,14 +114,72 @@ async def _(data: Message):
     text += '\n\n'
 
     for r, l in reward_list.items():
+
         if r == 0:
             rewards = int(wordle_config.rewards.golden * level_rate * rewards_rate)
             text += f'第一名获得{rewards}合成玉；\n'
+
         elif r == 1:
             rewards = int(wordle_config.rewards.silver * level_rate * rewards_rate)
             text += f'第二名获得{rewards}合成玉；\n'
+
         else:
             rewards = int(wordle_config.rewards.copper * level_rate * rewards_rate)
             text += f'第三名获得{rewards}合成玉；\n'
 
     return Chain(data, at=False).text('游戏结束').text('\n').text(text, auto_convert=False)
+
+
+@bot.on_group_message(function_id='wordle', keywords=['举报', '举办'])
+async def _(data: Message):
+    target = data.at_target
+    report_id = [item for item in target]
+    status = 0 if '清除' in data.text else 1
+
+    if status == 1:
+
+        for i in report_id:
+
+            if i not in config.admin.accounts:
+
+                if data.user_id == i:
+                    return Chain(data).text('举报失败！\n您不能举报自己。')
+
+                else:
+                    date = datetime.date.today()
+
+                    if ReportRecord.get_or_none(
+                        ReportRecord.source_id == data.user_id, ReportRecord.target_id == i, ReportRecord.date == date
+                    ) is not None:
+
+                        return Chain(data).text('您今天已经举报过该用户。')
+
+                    else:
+                        UserInfo.update(
+                            times_report=UserInfo.times_report + 1
+                        ).where(
+                           UserInfo.user_id == i
+                        ).execute()
+
+                        ReportRecord.insert(
+                            source_id=data.user_id,
+                            target_id=i,
+                            date=date
+                        ).execute()
+
+                        return Chain(data).text(f'举报{report_id}成功！\n请注意，若恶意举报将可能被永久封禁，请确保您的举报真实有效。')
+
+            else:
+                return Chain(data).text('举报失败！\n无法举报管理员。')
+
+    else:
+        if data.is_admin:
+
+            for i in report_id:
+
+                UserInfo.update(times_report=0).where(UserInfo.user_id == i).execute()
+
+                return Chain(data).text(f'{report_id}已清除举报。')
+
+        else:
+            return None
