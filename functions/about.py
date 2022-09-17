2ff226711012a634084f61105b20f95de28fad5a
Vivien <34387011+vivien8261@users.noreply.github.com>
2022-09-17 17:56:22 +0800

    update

--- functions/about.py
+++ functions/about.py
@@ -3,7 +3,7 @@ from core import bot, Message, Chain
 
 @bot.on_message(keywords=['功能', '帮助', '说明', 'help'], allow_direct=True)
 async def _(data: Message):
-    with open('functions/_docs/function.md', mode='r', encoding='utf-8') as file:
+    with open('config/readme.md', mode='r', encoding='utf-8') as file:
         doc = file.read()
     return Chain(data).html('template/function/function.html', {'doc': doc}, render_time=1000)
 
