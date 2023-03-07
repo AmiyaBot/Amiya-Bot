5c6adc4b933b61ba85f6daff4adf16e93fa0c66f
root <hsyhhssyy@gmail.com>
2023-03-07 21:23:31 +0800

    本地提交remote

--- run_local_in_docker.sh
+++ run_local_in_docker.sh
@@ -18,7 +18,7 @@ do
 done
 
 # 创建镜像，在本地配置文件修改完毕的情况下，执行下面的命令打包镜像
-docker build -t amiya-bot .
+docker build --no-cache -t amiya-bot .
 
 rm docker_plugin_requirements.txt
 
@@ -28,7 +28,7 @@ then
     docker build -t amiya-bot-v2raya .
     docker stop amiya-bot
     docker rm amiya-bot
-    docker run -dit --name amiya-bot --restart=always --ip 172.172.0.10 -p 5080:5080 -p 5443:5443 -v /opt/amiya-bot/amiya-bot-v6/plugins:/amiyabot/plugins -v /opt/amiya-bot/amiya-bot-v6/resource:/amiyabot/resource amiya-bot-v2raya /amiyabot/docker_start.sh
+    docker run -dit --name amiya-bot --restart=always --cap-add NET_ADMIN  --ip 172.172.0.10 -p 5080:5080 -p 5443:5443 -v /opt/amiya-bot/amiya-bot-v6/plugins:/amiyabot/plugins -v /opt/amiya-bot/amiya-bot-v6/resource:/amiyabot/resource amiya-bot-v2raya /amiyabot/docker_start.sh
 else
     # 然后执行下面的命令来启动阿米娅bot
     docker stop amiya-bot
--- v2raya/docker_start.sh
+++ v2raya/docker_start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /usr/bin/v2raya --log-disable-timestamp &
+python3.8 /etc/v2raya/monitor.py &
 python3.8 /amiyabot/amiya.py
