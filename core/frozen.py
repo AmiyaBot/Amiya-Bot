7a1624f017f304193b039e0f5bb0c89a873030f8
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-03-14 19:38:08 +0800

    update 修复数据库引用

--- core/frozen.py
+++ core/frozen.py
@@ -4,3 +4,7 @@ import dhash
 from . import server
 from jieba import posseg
 from core.lib import baiduCloud
+from core.database import bot
+from core.database import group
+from core.database import messages
+from core.database import user
