39d7331fc2603eecefec5f5c816559a0e7d7b519
Songyang.Huo <hsyhhssyy@users.noreply.github.com>
2023-03-16 12:11:15 +0800

    ChatGPT说这个格式符合PEP8

--- core/lib/pluginExtension.py
+++ core/lib/pluginExtension.py
@@ -1,29 +1,28 @@
 from core.database.plugin import PluginConfiguration
-from amiyabot import (
-    PluginInstance,
-    log
-)
+from amiyabot import PluginInstance, log
 
-def get_channel_config(self:PluginInstance,channel_id:str):
-    if channel_id is None or channel_id == "0":
-        log.error(f"{self.plugin_id}:Try get channel config with None channel id!")
+
+def get_channel_config(self: PluginInstance, channel_id: str):
+    if channel_id is None or channel_id == '0':
+        log.error(f"{self.plugin_id}: Try get channel config with None channel id!")
         return "{}"
-    
-    confStr = PluginConfiguration.get_or_none(plugin_id=self.plugin_id,channel_id=channel_id)
 
-    if confStr is None:
+    conf_str = PluginConfiguration.get_or_none(plugin_id=self.plugin_id, channel_id=channel_id)
+
+    if conf_str is None:
         # try return default
-        if hasattr(self, "default_channel_config"):
+        if hasattr(self, 'default_channel_config'):
             return self.default_channel_config
         return "{}"
-    
-    return confStr.json_config
 
-def set_channel_config(self:PluginInstance,channel_id:str,json:str):
-    if channel_id is None or channel_id == "0":
-        log.error(f"{self.plugin_id}:Try set channel config with None channel id!")
+    return conf_str.json_config
+
+
+def set_channel_config(self: PluginInstance, channel_id: str, json: str):
+    if channel_id is None or channel_id == '0':
+        log.error(f"{self.plugin_id}: Try set channel config with None channel id!")
         return
-    
+
     data = {
         'plugin_id': self.plugin_id,
         'channel_id': channel_id,
@@ -31,57 +30,61 @@ def set_channel_config(self:PluginInstance,channel_id:str,json:str):
         'version': self.version
     }
 
-    confStr = PluginConfiguration.get_or_none(plugin_id=self.plugin_id,channel_id=channel_id)
+    conf_str = PluginConfiguration.get_or_none(plugin_id=self.plugin_id, channel_id=channel_id)
 
-    if confStr == None:
+    if conf_str is None:
         PluginConfiguration.create(
             plugin_id=self.plugin_id,
             channel_id=channel_id,
             json_config=json,
             version=self.version
         )
-        log.info(f"{self.plugin_id}:Config Insert!")
+        log.info(f"{self.plugin_id}: Config Insert!")
     else:
-        confStr.json_config=json
-        confStr.version=self.version
-        confStr.save()
-        log.error(f"{self.plugin_id}:Config Update!")
+        conf_str.json_config = json
+        conf_str.version = self.version
+        conf_str.save()
+        log.error(f"{self.plugin_id}: Config Update!")
 
-def get_global_config(self:PluginInstance):
 
-    confStr = PluginConfiguration.get_or_none(plugin_id=self.plugin_id,channel_id="0")
+def get_global_config(self: PluginInstance):
+    conf_str = PluginConfiguration.get_or_none(plugin_id=self.plugin_id, channel_id='0')
 
-    if confStr is None:
+    if conf_str is None:
         # try return default
-        if hasattr(self, "default_global_config"):
+        if hasattr(self, 'default_global_config'):
             return self.default_global_config
         return "{}"
-    
-    return confStr.json_config
 
-def set_global_config(self:PluginInstance,json:str):
-    confStr = PluginConfiguration.get_or_none(plugin_id=self.plugin_id,channel_id="0")
+    return conf_str.json_config
 
-    if confStr == None:
+
+def set_global_config(self: PluginInstance, json: str):
+    conf_str = PluginConfiguration.get_or_none(plugin_id=self.plugin_id, channel_id='0')
+
+    if conf_str is None:
         PluginConfiguration.create(
             plugin_id=self.plugin_id,
-            channel_id="0",
+            channel_id='0',
             json_config=json,
             version=self.version
         )
-        log.info(f"{self.plugin_id}:Config Insert!")
+        log.info(f"{self.plugin_id}: Config Insert!")
     else:
-        confStr.json_config=json
-        confStr.version=self.version
-        confStr.save()
-        log.error(f"{self.plugin_id}:Config Update!")
+        conf_str.json_config = json
+        conf_str.version = self.version
+        conf_str.save()
+        log.error(f"{self.plugin_id}: Config Update!")
+
 
 PluginInstance.get_channel_config = get_channel_config
 PluginInstance.set_channel_config = set_channel_config
 PluginInstance.get_global_config = get_global_config
 PluginInstance.set_global_config = set_global_config
 
-# del(get_channel_config)
-# del(set_channel_config)
-# del(get_global_config)
-# del(set_global_config)
+
+
+del get_channel_config
+del set_channel_config
+del get_global_config
+del set_global_config
