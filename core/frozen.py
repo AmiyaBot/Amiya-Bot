743165b655fa652c652aea700c56aebba12907bb
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-08-02 18:42:10 +0800

    fix build

--- core/frozen.py
+++ core/frozen.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import srsly
 import dhash
 import requests_html
@@ -9,3 +11,13 @@ from core.database import bot
 from core.database import group
 from core.database import messages
 from core.database import user
+
+from amiyabot.builtin.lib.imageCreator import FontStyle
+from amiyabot.builtin.messageChain import ChainConfig
+
+if hasattr(sys, 'frozen'):
+    DIR_PATH = os.path.dirname(os.path.realpath(sys.executable))
+
+    FontStyle.file = os.path.join(DIR_PATH, '_assets/font/HarmonyOS_Sans_SC.ttf')
+    ChainConfig.md_template = os.path.join(DIR_PATH, '_assets/markdown/template.html')
+    ChainConfig.md_template_dark = os.path.join(DIR_PATH, '_assets/markdown/template-dark.html')
